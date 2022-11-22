8abf77cc1af38e91d5e4ee83181f4ea60efd0e5c
pndayo1404 <tranvanphucnguyen12@gmail.com>
2022-11-22 23:15:17 +1100

    Finished 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let str1 = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        str1 += str[i];
+    }
+    return str1;
 };
 
 // Do not edit below this line
