5c3154267978184aa3385de7a416dcc8f571a6f4
pndayo1404 <tranvanphucnguyen12@gmail.com>
2022-11-22 21:44:57 +1100

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let str1 = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++){
+        str1 += str;
+    }
+    return str1;
 };
 
 // Do not edit below this line
